746aa1a20a149d349d38a2a8d75c2cbae186ff53
PunnyaPS <punnyamridul@gmail.com>
2026-07-01 18:45:41 +0530

    Time: 0 ms (100%), Space: 147.9 MB (100%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.dart
@@ -0,0 +1,16 @@
+class Solution {
+
+  int addDigits(int num) {
+    if (num<10){
+        return num;
+    }
+    int sum=0;
+    while( num>0){
+        sum+= num%10;
+        num~/=10;
+
+    }
+    
+    return (sum<10)? sum : addDigits(sum);
+  }
+}
