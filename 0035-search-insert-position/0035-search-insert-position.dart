3a953470da08141da4f9177b49fa5e150313fdc8
PunnyaPS <punnyamridul@gmail.com>
2026-08-05 09:46:45 +0530

    Time: 0 ms (100%), Space: 148.1 MB (80.41%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int searchInsert(List<int> nums, int target) {
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>=target){
+            return i;
+        }
+
+    }
+    return nums.length;
+  }
+}
